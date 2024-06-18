a28b095dde9f2d244ca4d71c59b54a9ac1b37ff8
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-18 15:31:37 +0800

    Update freecodecamp_react.js

--- freecodecamp_react.js
+++ freecodecamp_react.js
@@ -1,4 +1,54 @@
+// USING MAP AND FILTER 2
 
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          username: 'Jeff',
+          online: true
+        },
+        {
+          username: 'Alan',
+          online: false
+        },
+        {
+          username: 'Mary',
+          online: true
+        },
+        {
+          username: 'Jim',
+          online: false
+        },
+        {
+          username: 'Sara',
+          online: true
+        },
+        {
+          username: 'Laura',
+          online: true
+        }
+      ]
+    };
+  }
+
+  render() {
+    const usersOnline = this.state.users.filter(
+      item=>{return item.online}
+      );
+
+    const renderOnline = usersOnline.map(
+      item=>{return <li key={item.username}>{item.username}</li>}
+      );
+
+    return (
+      <div>
+        <h1>Current Online Users:</h1>
+        <ul>{renderOnline}</ul>
+      </div>
+    );}
+}
 
 // GIVING LIST ITEMS IN <li></li> A UNIQUE KEY VALUE
 // https://legacy.reactjs.org/docs/lists-and-keys.html
