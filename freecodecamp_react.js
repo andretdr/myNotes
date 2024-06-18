d957b56b0cda7219bf483a91a10694b7d258587d
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-18 15:05:29 +0800

    Update freecodecamp_react.js

--- freecodecamp_react.js
+++ freecodecamp_react.js
@@ -1,3 +1,57 @@
+// USING ARRAY SPLIT AND MAP
+
+const textAreaStyles = {
+  width: 235,
+  margin: 5
+};
+
+class MyToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userInput: '',
+      toDoList: []
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit() {
+
+    // SPLIT
+    const itemsArray = this.state.userInput.split(',');
+    this.setState({
+      toDoList: itemsArray
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value
+    });
+  }
+  render() {
+
+    // MAP
+    const items = this.state.toDoList.map(
+      item=>{ return <li>{item}</li>});
+    return (
+      <div>
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.userInput}
+          style={textAreaStyles}
+          placeholder='Separate Items With Commas'
+        />
+        <br />
+        <button onClick={this.handleSubmit}>Create List</button>
+        <h1>My "To Do" List:</h1>
+        <ul>{items}</ul>
+      </div>
+    );}
+}
+
+
 // Changing style CONDITIONALLY
 
 class GateKeeper extends React.Component {
