73aa9b108c9661c2f164216bcff038850904cffc
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-18 22:54:53 +0800

    Update freecodecamp_bootstrap.js

--- freecodecamp_bootstrap.js
+++ freecodecamp_bootstrap.js
@@ -1,1 +1,22 @@
 
+// https://www.w3schools.com/bootstrap5/bootstrap_buttons.php
+// BUTTONS PRIMARY IS main app default button COLOR
+<button class="btn btn-block btn-primary">Like</button>
+
+// BUTTONS BLOCK LEVEL, width 100%
+<button class="btn btn-default btn-block">Like</button>
+
+// BUTTONS CLASS
+<button class="btn btn-default">Like</button>
+
+// TEXT CENTER
+<h2 class="red-text text-center">
+
+// RESPONSIVE IMGS
+<img src="https:/running-cats.jpg" class="img-responsive" alt="Three kittens">
+
+// FLUID CONTAINERS
+<div class="container-fluid">
+  ...
+
+<div>
