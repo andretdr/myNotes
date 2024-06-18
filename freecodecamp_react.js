5649b4789da8749c39a431cfd9ea141626b92ed6
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-18 12:01:45 +0800

    Update freecodecamp_react.js

--- freecodecamp_react.js
+++ freecodecamp_react.js
@@ -1,3 +1,66 @@
+// TERNARY OPERATOR
+
+const inputStyle = {
+  width: 235,
+  margin: 5
+};
+
+class CheckUserAge extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: '',
+      userAge: ''
+    }
+
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+      userAge: ''
+    });
+  }
+
+  submit() {
+    this.setState(state => ({
+      userAge: state.input
+    }));
+  }
+
+  render() {
+
+    const buttonOne = <button onClick={this.submit}>Submit</button>;
+    const buttonTwo = <button>You May Enter</button>;
+    const buttonThree = <button>You Shall Not Pass</button>;
+
+    console.log(this.state.userAge);
+
+    return (
+      <div>
+        <h3>Enter Your Age to Continue</h3>
+        <input
+          style={inputStyle}
+          type='number'
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+        <br />
+
+      // HEREEEEEEEEEEEEEEEEE
+          
+      {!this.state.userAge ? buttonOne : 
+      ((this.state.userAge >= 18) ? buttonTwo : buttonThree)}
+      </div>
+    );
+  }
+}
+
+
+
 // && CONDITIONAL MARKUP
 
 class MyComponent extends React.Component {
