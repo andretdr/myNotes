a9067e935cc84b4802de9542339de9cf41870c90
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-18 10:54:44 +0800

    Update freecodecamp_react.js

--- freecodecamp_react.js
+++ freecodecamp_react.js
@@ -1,3 +1,54 @@
+
+
+// SHOULDCOMPONENTUPDATE, DIDUPDATE
+
+class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should I update?');
+    if (nextProps.value % 2 === 0)
+        return true;
+    else
+        return false;
+  }
+
+  componentDidUpdate() {
+    console.log('Component re-rendered.');
+  }
+
+  render() {
+    return <h1>{this.props.value}</h1>;
+}}
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+    this.addValue = this.addValue.bind(this);
+  }
+
+  addValue() {
+    this.setState(state => ({
+      value: state.value + 1
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value} />
+      </div>
+    );
+}}
+
+
+
 // Add/ remove Event Listeners to COMPONENTDIDMOUNT n 
 // WILLUNMOUNT CLEANUP METHOD
 // https://www.youtube.com/watch?v=AZXJzYbmgm0
