807d52b1ad151369a6100f988b5f1f3bab64d372
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-18 14:29:07 +0800

    Update freecodecamp_react.js

--- freecodecamp_react.js
+++ freecodecamp_react.js
@@ -1,3 +1,45 @@
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>{
+      this.props.expression
+        ? 'You Win!'
+        : 'You Lose!'
+        }</h1>;
+}}
+
+class GameOfChance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick() {
+  // THIS IS A DIFFERENT WAY TO SET THE STATE FROM PREVIOUS, BUT IT WORKS
+    this.setState(prevState => {
+      return {
+        counter: prevState.counter+1
+      }
+  });
+  }
+  
+  render() {
+    const expression = Math.random() >= .5; // Change this line
+    return (
+      <div>
+        <button onClick={this.handleClick}>Play Again</button>
+        <Results fiftyFifty = {expression}/>
+        <p>{'Turn: ' + this.state.counter}</p>
+      </div>
+    );
+}}
+
+
 // TERNARY OPERATOR
 
 const inputStyle = {
