24732f7f23fcdab0cc9014fdd1e4aeb3c4ba31d3
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-19 19:51:44 +0800

    Update freecodecamp_jquery.js

--- freecodecamp_jquery.js
+++ freecodecamp_jquery.js
@@ -1,10 +1,13 @@
 
-
+// .addCLASS
+// $() SELECTORS, like document.querySelector()
 // ADDING CLASS TO ELEMENT, BOUNCE CLASS TO ALL BUTTONS
 
 <script>
   $(document).ready(function() {
     $('button').addClass('animated bounce');
+    $('.well').addClass('animated shake');
+    $('#text-primary').addClass('animated shake');
   });
 </script>
 .... html ...
