b500ab1313be25f893bad15b5ba378e71e45d79f
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-19 19:47:42 +0800

    Create freecodecamp_jquery.js

---
+++ freecodecamp_jquery.js
@@ -0,0 +1,19 @@
+
+
+// ADDING CLASS TO ELEMENT, BOUNCE CLASS TO ALL BUTTONS
+
+<script>
+  $(document).ready(function() {
+    $('button').addClass('animated bounce');
+  });
+</script>
+.... html ...
+
+
+// INITIALISING AT TOP OF PAGE
+// ALL JQUERY STARTS WITH $ or BLING
+
+<script>
+  $(document).ready(function() {
+  });
+</script>
