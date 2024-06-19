cd88d535ebe2c26249a70c42330c2de0fa3033a7
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-19 23:00:21 +0800

    Update freecodecamp_sass.js

--- freecodecamp_sass.js
+++ freecodecamp_sass.js
@@ -1,4 +1,36 @@
+// COMBINING STR W VAR AND MATH OPERATIONS
+// for $var from start TO end (end exclusive), for $var from start THROUGH end (end inclusive)
+// FOR LOOP
 
+// col-#{$i} combines the string w var, 1-12
+@for $i from 1 through 12 {
+  .col-#{$i} { width: 100%/12 * $i; }
+}
+
+// 1-5, calculations are also possible
+@for $j from 1 to 6 {
+  .text-#{$j} { font-size: 15px * $j }
+}
+
+
+
+// if then else
+// ALL SASS OCCURS IN STYLE TAG 
+
+@mixin text-effect($val) {
+  @if $val == danger {
+    color: red;
+  }
+  @else if $val == alert {
+    color: yellow;
+  }
+  @else if $val == success {
+    color: green;
+  }
+  @else {
+    color: black;
+  }
+}
 
 
 // MIXIN
@@ -42,6 +74,8 @@ article {
 
 
 // SASS USES VARIABLES
+// ALL SASS OCCURS IN STYLE TAG 
+
 <style type='text/scss'>
 
 $main-fonts: Arial, sans-serif;
