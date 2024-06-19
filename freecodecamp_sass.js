43361d2f3cd38fbabe9dbaa0bda4dc36fb97d4f0
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-19 22:40:57 +0800

    Update freecodecamp_sass.js

--- freecodecamp_sass.js
+++ freecodecamp_sass.js
@@ -1,6 +1,32 @@
 
 
 
+// MIXIN
+// SOMETHING LIKE FUNCTIONS?
+
+// we want to simplify this
+div {
+  -webkit-box-shadow: 0px 0px 4px #fff;
+  -moz-box-shadow: 0px 0px 4px #fff;
+  -ms-box-shadow: 0px 0px 4px #fff;
+  box-shadow: 0px 0px 4px #fff;
+}
+
+// mixin
+@mixin box-shadow($x, $y, $blur, $c){ 
+  -webkit-box-shadow: $x $y $blur $c;
+  -moz-box-shadow: $x $y $blur $c;
+  -ms-box-shadow: $x $y $blur $c;
+  box-shadow: $x $y $blur $c;
+}
+
+// to use, @include
+div {
+  @include box-shadow(0px, 0px, 4px, #fff);
+}
+
+
+
 // NESTING
 article {
   height: 200px;
@@ -13,6 +39,7 @@ article {
     color: blue;
   }
 }
+
 
 // SASS USES VARIABLES
 <style type='text/scss'>
