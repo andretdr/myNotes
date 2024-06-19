b802daadbfb6c096e4a33e76856063cc1e547d4e
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-19 20:02:36 +0800

    Update freecodecamp_jquery.js

--- freecodecamp_jquery.js
+++ freecodecamp_jquery.js
@@ -1,6 +1,25 @@
+// .html(), like .innerHTML
+// CHANGING INNERHTML
 
-// .addCLASS
-// $() SELECTORS, like document.querySelector()
+$("h3").html("<em>jQuery Playground</em>");
+
+// .prop()
+// CHANGING NON CSS PROPERTIES
+
+$('button').prop('disabled', true);
+
+// .css()
+// CSS CHANGING
+
+<script>
+  $(document).ready(function() {
+    $('#target1').css('color', 'blue');
+  });
+</script>
+
+
+// .addCLASS, .removeCLASS
+// $() SELECTORS, like document.querySelector(), type, class n button
 // ADDING CLASS TO ELEMENT, BOUNCE CLASS TO ALL BUTTONS
 
 <script>
@@ -8,6 +27,7 @@
     $('button').addClass('animated bounce');
     $('.well').addClass('animated shake');
     $('#text-primary').addClass('animated shake');
+    $("button").removeClass("btn-default");
   });
 </script>
 .... html ...
