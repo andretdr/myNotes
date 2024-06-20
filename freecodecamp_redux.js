5842a968ed2fbd6f2b0dc9218a3bdaee24e34257
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-20 11:32:31 +0800

    Update freecodecamp_redux.js

--- freecodecamp_redux.js
+++ freecodecamp_redux.js
@@ -108,46 +108,67 @@ const defaultState = {
   });
 
 // ================================
+  
+  const INCREMENT = 'INCREMENT';
+  const DECREMENT = 'DECREMENT';
+  
+  const counterReducer = (state = 0, action) => {
+    switch(action.type) {
+      case INCREMENT:
+        return state + 1;
+      case DECREMENT:
+        return state - 1;
+      default:
+        return state;
+    }};
+  
+  const LOGIN = 'LOGIN';
+  const LOGOUT = 'LOGOUT';
+  
+  const authReducer = (state = {authenticated: false}, action) => {
+    switch(action.type) {
+      case LOGIN:
+        return {
+          authenticated: true
+        }
+      case LOGOUT:
+        return {
+          authenticated: false
+        }
+      default:
+        return state;
+    }};
+  
+  const rootReducer = Redux.combineReducers({
+    counter: counterReducer,
+    auth: authReducer
+  })
+  
+  const store = Redux.createStore(rootReducer);
 
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
 
-const counterReducer = (state = 0, action) => {
-  switch(action.type) {
-    case INCREMENT:
-      return state + 1;
-    case DECREMENT:
-      return state - 1;
-    default:
-      return state;
+// SENDING DATA IN ACTIONS
+// JUST ADD THE DATA IN THE ACTION OBJECT OR ACTION CREATOR
+  
+  const ADD_NOTE = 'ADD_NOTE';
+  
+  // ACTION CREATOR
+  const addNoteText = (note) => { return {type: ADD_NOTE, text: note}};
+  
+  // REDUCER
+  const notesReducer = (state = 'Initial State', action) => {
+    switch(action.type) {
+      case ADD_NOTE:
+        return (action.text)
+      default:
+        return state;
   }};
-
-const LOGIN = 'LOGIN';
-const LOGOUT = 'LOGOUT';
-
-const authReducer = (state = {authenticated: false}, action) => {
-  switch(action.type) {
-    case LOGIN:
-      return {
-        authenticated: true
-      }
-    case LOGOUT:
-      return {
-        authenticated: false
-      }
-    default:
-      return state;
-  }};
-
-const rootReducer = Redux.combineReducers({
-  counter: counterReducer,
-  auth: authReducer
-})
-
-const store = Redux.createStore(rootReducer);
-
-
-
+  
+  const store = Redux.createStore(notesReducer);
+  
+  console.log(store.getState());
+  store.dispatch(addNoteText('Hello!'));
+  console.log(store.getState());
 
 
 
