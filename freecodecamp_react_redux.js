f3dd5470a85a23020385509bd934b9104049cb03
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-20 17:58:57 +0800

    Update freecodecamp_react_redux.js

--- freecodecamp_react_redux.js
+++ freecodecamp_react_redux.js
@@ -162,10 +162,84 @@ class AppWrapper extends React.Component {
     )}};
 
 
+==============================================
+// USING REDUX DONT NEED LOCAL STATE IN REACT
+==============================================
+// CHANGE IT SUCH THAT message is now handled by REDUX, not REACT
 
+// Redux:
+const ADD = 'ADD';
 
+const addMessage = (message) => {
+  return {type: ADD, message: message}};
 
+const messageReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [...state, action.message];
+    default:
+      return state;
+  }};
 
+const store = Redux.createStore(messageReducer);
+
+// React:
+const Provider = ReactRedux.Provider;
+const connect = ReactRedux.connect;
+
+class Presentational extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      // REMOVED - MESSAGE: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  submitMessage() {
+      // ADDED THIS LINE
+      this.props.submitNewMessage(this.state.input);
+    this.setState({input: ''})
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        <input value={this.state.input} onChange={this.handleChange}/><br/>
+        <button onClick={this.submitMessage}>Submit</button>
+        <ul>
+          {this.props.messages.map( (message, idx) => {return (
+            <li key={idx}>{message}</li>
+            )})}
+        </ul>
+      </div>
+    );}};
+
+const mapStateToProps = (state) => {
+  return {messages: state}
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {submitNewMessage: (message) => {
+      dispatch(addMessage(message))}}};
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(Presentational);
+
+class AppWrapper extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Container/>
+      </Provider>
+    );}};
 
 
 
