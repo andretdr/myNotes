da0bdd13f6cb98f6808bc70bb0a3d61fcd635086
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-24 13:51:15 +0800

    Update l_react_router.js

--- l_react_router.js
+++ l_react_router.js
@@ -1,2 +1,24 @@
 // https://hygraph.com/blog/routing-in-react
 // https://www.youtube.com/watch?v=oTIJunBa6MA
+
+// install 
+npm install react-router-dom@6
+
+// replace main render and import
+// https://reactrouter.com/en/main/routers/picking-a-router
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter([
+    {path: '/', element: <HomePage />},
+    {path: '/profiles', element: <Profiles />}
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+
+    <Router.Provider router={router} />
+  
+)
