e8abf62e5cddf261499407f44a15d351fb4b2e9f
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-25 10:20:52 +0800

    Create system_github.js

---
+++ system_github.js
@@ -0,0 +1,7 @@
+// GITHUB
+
+// CLONE REPO
+git clone https://github.com/iamshaunjp/bootstrap-5-tutorial.git
+
+// CLONE BRANCH ONLY
+git clone -b lesson-1 https://github.com/iamshaunjp/bootstrap-5-tutorial.git
