d09723cd15f9ee3e76536a0e6779f7a0cf7a191c
andretdr <162653875+andretdr@users.noreply.github.com>
2024-06-27 12:15:58 +0800

    Update g_regex.js

--- g_regex.js
+++ g_regex.js
@@ -1,3 +1,5 @@
+// https://www.youtube.com/watch?v=sa-TUpSx1JA
+
 # regex = /asd/
 ####################
 
@@ -7,6 +9,9 @@
 // backslash
   \. \[ \{ \\ 
 
+# REGEX
+########################
+    
 .      // Any Character EXCEPT new line
 \d      // Digit (0-9)
 \D      // Not a digit (0-9)
@@ -15,11 +20,53 @@
 \s      // Whitespace (space, tab, newline)
 \S      // Not whitespace
 
-\b      // Word Boundary
+\b      // Word Boundary matches the start or end of a word. \bha will match haga, not gha. \bha\b will only match ha not haha
 \B      // Not word boundary
-^      // Begining of a String
-$      // End of a String
+^      // Begining of a String. ^ha will match 'ha we kjalwkejawe', not match 'ad ha asdladh ha'
+$      // End of a String. ha$ will match 'sh ha asdha'
+
+# CHARACTER SETS
+#########################
+// DONT NEED TO ESCAPE CHARS HERE
+// each item above, or char set below matches only 1 character
+
 
 []        // Matches Chars in bracket
-[^ ]      // Matches Chats NOT in bracket
+[^ ]      // Matches Chars NOT in bracket
 |        // Either Or
+( )      // Group
+  
+  
+321-223-1234
+543.123.6421
+\d\d\d[.-]\d\d\d[.-]\d\d\d\d
+
+543..123--3421
+\d\d\d[.-][.-]\d\d\d[.-][.-]\d\d\d\d
+
+1234567
+[1-7]
+
+all letters
+[a-zA-Z]
+
+all NOT letters
+[^a-zA-Z]
+matches everything NOT in the set
+
+# QUANTIFIERS
+###############
+
+*        // 0 or more
++        // 1 or more
+?        // 0 or 1
+{3}      // exact number
+{3,4}    // range of numbers (min, max)
+
+
+
+
+
+
+
+
