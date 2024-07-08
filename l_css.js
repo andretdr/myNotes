e4217e9bf3f1408a21443d2017213924e0583921
andretdr <162653875+andretdr@users.noreply.github.com>
2024-07-08 14:45:38 +0800

    Create l_css.js

---
+++ l_css.js
@@ -0,0 +1,17 @@
+// em vs rem
+
+em is about 16px
+
+// fonts
+/////////
+// em for fonts looks at the font size in the immediate parent
+// rem for fonts looks at the HTML (root) font size
+
+// margins and padding
+///////////////////////
+// em for margins and paddings looks towards the CURRENT level's font size
+// This is useful for example, making buttons where all the margins an paddings are in relation to each other, 
+// and just changing the parent font size scales the buttons correctly
+
+// rem for margins and paddings looks towards the ROOT level's font size
+
