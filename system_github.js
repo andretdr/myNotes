6fc79faf2527217f6e093823d13532193b1afb82
andretdr <162653875+andretdr@users.noreply.github.com>
2024-07-10 17:34:53 +0800

    Update system_github.js

--- system_github.js
+++ system_github.js
@@ -1,7 +1,11 @@
 // GITHUB
 
 // CLONE REPO
-git clone https://github.com/iamshaunjp/bootstrap-5-tutorial.git
+ https://github.com/iamshaunjp/bootstrap-5-tutorial.git
 
 // CLONE BRANCH ONLY
 git clone -b lesson-1 https://github.com/iamshaunjp/bootstrap-5-tutorial.git
+
+// GITPAGES
+// have to run build so yr page is not dynamic
+npm run build
