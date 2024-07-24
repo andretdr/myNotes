53b302a2116093f613f810d636bfe58db62204e7
andretdr <162653875+andretdr@users.noreply.github.com>
2024-07-24 11:48:46 +0800

    Update and rename system_github.js to system_git.js

---
+++ system_git.js
@@ -0,0 +1,37 @@
+/////////////////
+// GIT CMD
+/////////////////
+
+
+// INSTALL
+///////////////
+// https://www.git-scm.com/
+
+
+// VERSION
+//////////////
+git --version
+
+
+// CONFIG
+//////////////
+git config --global user.name "w3schools-test"
+git config --global user.email "test@w3schools.com"
+
+
+// 
+
+
+
+
+// GIT
+
+// CLONE REPO
+ https://github.com/iamshaunjp/bootstrap-5-tutorial.git
+
+// CLONE BRANCH ONLY
+git clone -b lesson-1 https://github.com/iamshaunjp/bootstrap-5-tutorial.git
+
+// GITPAGES
+// have to run build so yr page is not dynamic
+npm run build
