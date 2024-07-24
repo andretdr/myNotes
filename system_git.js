0f3445a4468c206e06f4c3fa685fbb646f529915
andretdr <162653875+andretdr@users.noreply.github.com>
2024-07-24 12:51:43 +0800

    Update system_git.js

--- system_git.js
+++ system_git.js
@@ -1,7 +1,7 @@
 /////////////////
 // GIT CMD
 /////////////////
-
+// https://www.w3schools.com/git/
 
 // INSTALL
 ///////////////
@@ -39,7 +39,6 @@ git status
 git status --short M index.html
 
 
-
 // ADD
 /////////
 // add untracked files to staging area
@@ -59,6 +58,59 @@ git commit -m 'First release of hello world'
 // can commit without staging. NOT reccomended
 git commit -a -m 'auto commit change to hello world'
 
+
+// LOG
+/////////
+// commit history log
+git log
+
+
+// HELP
+/////////
+git help --all  // See all possible commands
+git command -help // See all the available options for the specific command
+
+git commit -help
+git prune -help
+
+SHIFT + G to jump the end of the list
+Use 'q' to exit readermode
+
+
+/////////////
+// BRANCH
+/////////////
+// creates a branch called hello-world-images from your current branch
+git branch hello-world-images
+
+// creates a branch called hello-world-images from master branch
+git branch hello-world-images master
+
+// check branch status
+git branch
+
+// MOVE to branch
+git checkout hello-world-images
+
+// -b option on checkout will create a new branch, and move to it, if it does not exist
+git checkout -b new-hello-world-images
+
+// Do your changes, stage and commit
+
+
+///////////
+// MERGE
+///////////
+// checkout to master first, then run merge on your branch 'emergency-fix TO master'
+git merge emergency-fix
+
+// DELETE branch after merging
+git branch -d emergency-fix
+
+
+// MERGE CONFLICT
+////////////////////
+// handle manually, then stage and commit the files affected
 
 
 
