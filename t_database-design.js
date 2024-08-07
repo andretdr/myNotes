9c85943a2ad9af890c95ff5e0085b1ddb6ad83c2
andretdr <162653875+andretdr@users.noreply.github.com>
2024-08-07 10:10:15 +0800

    Update t_database-design.js

--- t_database-design.js
+++ t_database-design.js
@@ -35,7 +35,6 @@ CREATE TABLE tests (
      REFERENCES students (student_id)
 );
 
-/////////////////
 // ALTER TABLE
 /////////////////
 CREATE TABLE tests (
@@ -50,4 +49,86 @@ ALTER TABLE tests
     REFERENCES students (student_id);
 
 
+/////////////////////////
+// RELATIONSHIP
+/////////////////////////
+
+// 1:1
+/////////
+CREATE TABLE students (
+    student_id integer PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+CREATE TABLE student_details (
+    student_id integer PRIMARY KEY,
+    address VARCHAR(200),
+    phone_number VARCHAR(20) UNIQUE,
+    FOREIGN KEY (student_id) REFERENCES students(student_id)
+);
+
+// student_id is both the primary key in student_details and a foreign key referencing students
+// foreign key needs to be unique
+// only possible w 1:1 relationships
+
+// 1:many
+///////////
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(100)
+);
+
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    department_id INT, // NOT unique, can have duplicates
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+// foreign key NOT unique, can have duplicates
+
+// many:1
+///////////
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    order_date DATE,
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+// foreign key NOT unique, can have duplicates
+// same as 1:many, just see it from the other entity
+
+many:many
+CREATE TABLE books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(200)
+);
+
+CREATE TABLE authors (
+    author_id INT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+// create joining table
+CREATE TABLE book_authors (
+    book_id INT,
+    author_id INT,
+    PRIMARY KEY (book_id, author_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id),
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+// if not using PRIMARY KEY (foreignkey, foreignkey), use UNIQUE to enforce uniqueness
+CREATE TABLE book_authors (
+    id SERIAL PRIMARY KEY,
+    book_id INT,
+    author_id INT,
+    FOREIGN KEY (book_id) REFERENCES books(book_id),
+    FOREIGN KEY (author_id) REFERENCES authors(author_id),
+    UNIQUE (book_id, author_id)
+);
 
