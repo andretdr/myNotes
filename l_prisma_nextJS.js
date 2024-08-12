8af522fa7172183675e5f69f6311ef8c9fa44e63
andretdr <162653875+andretdr@users.noreply.github.com>
2024-08-12 20:24:20 +0800

    Update l_prisma_nextJS.js

--- l_prisma_nextJS.js
+++ l_prisma_nextJS.js
@@ -1,1 +1,15 @@
+// PRISMA
+///////////
 
+// PRISMA SCEHMA
+
+// PRISMA CLIENT
+
+// WORKFLOW
+npx create-next-app --example hello-world prisma-introduction
+cd prisma-introduction
+npm install prisma --save-dev
+npm install @prisma/client
+
+// create prisma schema
+npx prisma init
