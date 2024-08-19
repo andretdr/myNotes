02b011b6b354a1104839994b5c866bbfd29c1de2
andretdr <162653875+andretdr@users.noreply.github.com>
2024-08-19 19:24:39 +0800

    Update g_industryPractices_heicoders.ts

--- g_industryPractices_heicoders.ts
+++ g_industryPractices_heicoders.ts
@@ -1,1 +1,32 @@
+// FORMS
+//////////
 
+// Forms or any component is a server component. Server components cannot use hooks, only client components
+// For validations, etc, you may need to use client components
+
+export default function RegisterPage() {
+  return (
+    <form>
+      <div>
+        <label>Name</label>
+        <input name='name'></input>
+      </div>
+      <div>
+        <label>Email</label>
+        <input name='email'></input>
+      </div>
+      <div>
+        <label>Password</label>
+        <input name='password'></input>
+      </div>
+    
+    </form>
+
+
+
+    
+  )
+
+
+  
+}
